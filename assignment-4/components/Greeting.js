8c18f0f08c55fe93b7ad7a295ae3640ca8a2c984
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-06-14 18:38:48 +0000

    added greeting file to display at the login page.

---
+++ assignment-4/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Greeting() {
+  return (
+    <View>
+      <Text>Jobizz</Text>
+      <Text>Welcome Back</Text>
+      <Text>Let's log in. Apply to jobs!</Text>
+    </View>
+  )
+}
